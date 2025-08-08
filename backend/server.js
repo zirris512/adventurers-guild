8ee4a364078dc31012877632bc955dff9c3ebcbd
Brent <zirris512@gmail.com>
2025-08-07 20:15:37 -0500

    add connection to db

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,12 @@ const db = require('./database/db-connector');
 const express = require('express');
 const app = express();
 
-
 // Middleware
 const cors = require('cors');
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(cors({ credentials: true, origin: '*' }));
 app.use(express.json()); // this is needed for post requests
 
-
-const PORT = 50101;
+const PORT = 10101;
 
 // ########################################
 // ########## ROUTE HANDLERS
@@ -32,84 +30,91 @@ const PORT = 50101;
 // READ ROUTES
 // Job SELECT query, pull table data.
 app.get('/jobs', async (req, res) => {
-    try{
-        //Create and execute job SELECT query for general page population.
-        
-        const jobsQuery = 'SELECT * FROM Jobs;';
-        const [allJobs] = await db.query(jobsQuery);
+  try {
+    //Create and execute job SELECT query for general page population.
 
-        // Send results to frontend.
-        res.status(200).json({allJobs});
-    } catch (error) {
-        console.error("Error executing Jobs query:", error);
-        
-        //Generic error code.
-        res.status(500).send("An error occurred while executing the Jobs database query.");
-    }
+    const jobsQuery = 'SELECT * FROM `Jobs`;';
+    const [rows] = await db.query(jobsQuery);
+
+    // Send results to frontend.
+    res.status(200).json({ rows });
+  } catch (error) {
+    console.error('Error executing Jobs query:', error);
+
+    //Generic error code.
+    res
+      .status(500)
+      .send('An error occurred while executing the Jobs database query.');
+  }
 });
 
 //Location SELECT query, pull table data.
 app.get('/locations', async (req, res) => {
-    try{
-        //Create and execute job SELECT query for general page population.
-        
-        const locationQuery = 'SELECT * FROM Locations;';
-        const [allLocations] = await db.query(locationQuery);
+  try {
+    //Create and execute job SELECT query for general page population.
 
-        // Send results to frontend.
-        res.status(200).json({allLocations});
-    } catch (error) {
-        console.error("Error executing Locations query:", error);
-        
-        //Generic error code.
-        res.status(500).send("An error occurred while executing the Locations database query.");
-    }
+    const locationQuery = 'SELECT * FROM Locations;';
+    const [allLocations] = await db.query(locationQuery);
+
+    // Send results to frontend.
+    res.status(200).json({ allLocations });
+  } catch (error) {
+    console.error('Error executing Locations query:', error);
+
+    //Generic error code.
+    res
+      .status(500)
+      .send('An error occurred while executing the Locations database query.');
+  }
 });
 
 // Rank SELECT query, pull table data.
 
-app.get('/ranks', async (req,res)=>{
-    try{
-        //Create and execute job SELECT query for general page population.
-        
-        const rankQuery = 'SELECT * FROM Ranks;'; 
-        const [allRanks] = await db.query(rankQuery);
+app.get('/ranks', async (req, res) => {
+  try {
+    //Create and execute job SELECT query for general page population.
 
-        // Send results to frontend.
-    res.status(200).json({allRanks});
-    } catch (error) {
-        console.error("Error executing Ranks query:", error);
-    
-        //Generic error code.
-        res.status(500).send("An error occurred while executing the allRanks database query.");
-}
+    const rankQuery = 'SELECT * FROM Ranks;';
+    const [allRanks] = await db.query(rankQuery);
+
+    // Send results to frontend.
+    res.status(200).json({ allRanks });
+  } catch (error) {
+    console.error('Error executing Ranks query:', error);
+
+    //Generic error code.
+    res
+      .status(500)
+      .send('An error occurred while executing the allRanks database query.');
+  }
 });
 
 //Adventure's SELECT query, pull table data.
-app.get('/adventurers', async (req,res)=>{
-    try{
-        //Create and execute job SELECT query for general page population.
-        
-        const advenQuery = 'SELECT * FROM Adventurers;'; 
-        const [allAdvens] = await db.query(advenQuery);
+app.get('/adventurers', async (req, res) => {
+  try {
+    //Create and execute job SELECT query for general page population.
 
-        // Send results to frontend.
-    res.status(200).json({allAdvens});
-    } catch (error) {
-        console.error("Error executing Adventurers query:", error);
-    
-        //Generic error code.
-        res.status(500).send("An error occurred while executing the allAdvens database query.");
-}
+    const advenQuery = 'SELECT * FROM Adventurers;';
+    const [allAdvens] = await db.query(advenQuery);
+
+    // Send results to frontend.
+    res.status(200).json({ allAdvens });
+  } catch (error) {
+    console.error('Error executing Adventurers query:', error);
+
+    //Generic error code.
+    res
+      .status(500)
+      .send('An error occurred while executing the allAdvens database query.');
+  }
 });
 
 //Adventurer_Jobs SELECT Query, pull table data.
-app.get('/adventurerJobs', async (req,res)=>{
-    try{
-        //Create and execute job SELECT query for general page population.
-        
-        const ajQuery = 
-        `SELECT 
+app.get('/adventurerJobs', async (req, res) => {
+  try {
+    //Create and execute job SELECT query for general page population.
+
+    const ajQuery = `SELECT 
             CONCAT(Adventurers.first_name, ' ', Adventurers.last_name) AS adventurer,
             CONCAT(Jobs.job_opener_first_name, ' ', Jobs.job_opener_last_name) AS job_opener,
             Adventurer_Jobs.adventurer_completed_job,
@@ -120,64 +125,70 @@ app.get('/adventurerJobs', async (req,res)=>{
             Jobs.job_ID
         FROM Adventurer_Jobs
             JOIN Adventurers ON Adventurer_Jobs.adventurer_ID = Adventurers.adventurer_ID
-            JOIN Jobs ON Jobs.job_ID = Adventurer_Jobs.job_ID;`; 
-        const [aj_table] = await db.query(ajQuery);
+            JOIN Jobs ON Jobs.job_ID = Adventurer_Jobs.job_ID;`;
+    const [aj_table] = await db.query(ajQuery);
 
-        // Send results to frontend.
-    res.status(200).json({aj_table});
-    } catch (error) {
-        console.error("Error executing Adventurer Jobs query:", error);
-    
-        //Generic error code.
-        res.status(500).send("An error occurred while executing the ajQuery database query.");
-}
+    // Send results to frontend.
+    res.status(200).json({ aj_table });
+  } catch (error) {
+    console.error('Error executing Adventurer Jobs query:', error);
+
+    //Generic error code.
+    res
+      .status(500)
+      .send('An error occurred while executing the ajQuery database query.');
+  }
 });
 
 //Database Reset Call
 app.post('/dbReset', async (req, res) => {
-    const { request } = req.body;
+  const { request } = req.body;
 
-    if (request === "RESET_DB") {
-        try {
-            const [response] = await db.query(`CALL RESET_DB();`);
-            res.status(201).json({ "message": "Database was reset", "data": response });
-        } catch (error) {
-            res.status(500).json({ "message": "Database reset failed" });
-        }
-    } else {
-        res.status(400).json({ "message": "RESET REQUEST FAILED." });
+  if (request === 'RESET_DB') {
+    try {
+      const [response] = await db.query(`CALL RESET_DB()`);
+      res.status(201).json({ message: 'Database was reset', data: response });
+    } catch (error) {
+      res.status(500).json({ message: 'Database reset failed' });
     }
+  } else {
+    res.status(400).json({ message: 'RESET REQUEST FAILED.' });
+  }
 });
 
 // Add a new location.
 app.post('/locations', async (req, res) => {
-    const {
-        solar_system,
-        celestial_body_name,
-        target_latitude,
-        target_longitude
-    } = req.body;
+  const {
+    solar_system,
+    celestial_body_name,
+    target_latitude,
+    target_longitude,
+  } = req.body;
 
-    const addLocationQuery = `CALL InsertLocation(?, ?, ?, ?)`;
+  const addLocationQuery = `CALL InsertLocation(?, ?, ?, ?)`;
 
-    try {
+  try {
     await db.query(addLocationQuery, [
-        solar_system,
-        celestial_body_name,
-        target_latitude,
-        target_longitude
+      solar_system,
+      celestial_body_name,
+      target_latitude,
+      target_longitude,
     ]);
 
     res.status(201).json({ message: 'Location added successfully' });
-    } catch (error) {
-        console.error('Error inserting location:', error);
-        res.status(500).json({ error: 'Failed to add location' });
-    }
-    });
+  } catch (error) {
+    console.error('Error inserting location:', error);
+    res.status(500).json({ error: 'Failed to add location' });
+  }
+});
 
 // ########################################
 // ########## LISTENER
 
 app.listen(PORT, function () {
-    console.log('Express started on http://classwork.engr.oregonstate.edu:' + PORT + '; press Ctrl-C to terminate.');
+  console.log(
+    'Express started on http://classwork.engr.oregonstate.edu:' +
+      PORT +
+      '; press Ctrl-C to terminate.',
+  );
 });
